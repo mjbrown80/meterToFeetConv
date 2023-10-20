5a942c39278483d2c1b206b2921e8536243b4d9c
Matt Brown <mattbrown@Matts-MacBook-Pro.local>
2023-10-20 12:26:58 -0400

    update2

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,20 +1,40 @@
+import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Metric converter!");
+        boolean continueWithAnotherNumber = true;
 
-        System.out.println("Please enter a number: ");
-        int userNumber = scanner.nextInt();
+        while(continueWithAnotherNumber) {
+            System.out.println("Welcome to the Metric converter!");
 
-        System.out.println("enter Meters to Feet(1) or Feet to Meters(2)");
-        int unit = scanner.nextInt();
+            System.out.println("Please enter a whole number: ");
+            int userNumber = scanner.nextInt();
 
-        if(unit == 1){
-            
+            System.out.println("enter Meters to Feet(1) or Feet to Meters(2)");
+            int unit = scanner.nextInt();
+            double finalResult = 0;
+            if (unit == 1) {
+                finalResult = userNumber * 3.281;
+                finalResult = Math.round((finalResult * .10) / .10);
+                System.out.println(userNumber + " Meter = " + finalResult+ " Feet.");
+
+            } else if (unit == 2) {
+                finalResult = userNumber / 3.281;
+                finalResult = Math.round((finalResult * .10) / .10);
+                System.out.println(userNumber + " Feet = " + finalResult + " Meters.");
+            } else {
+                System.out.println("Invalid entry please try again!");
+            }
+            System.out.println("Would you like to convert another number? ((1) for Yes or (2) for No");
+            int continueChoice = scanner.nextInt();
+
+            if (continueChoice == 0){
+                continueWithAnotherNumber = false;
+            }
         }
-
+        System.out.println("Thank you for using the Metric converter!");
     }
 }
