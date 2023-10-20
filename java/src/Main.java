ec0cd47c034db94343b07e808803b9ffbb7c05ed
Matt Brown <mattbrown@Matts-MacBook-Pro.local>
2023-10-20 11:27:17 -0400

    update 1

---
+++ java/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Metric converter!");
+
+        System.out.println("Please enter a number: ");
+        int userNumber = scanner.nextInt();
+
+        System.out.println("enter Meters to Feet(1) or Feet to Meters(2)");
+        int unit = scanner.nextInt();
+
+        if(unit == 1){
+            
+        }
+
+    }
+}
